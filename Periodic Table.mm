5215b44390cb270a24d1d256d2957f77f68ab133
Somya champawat <somyachampawat.2020cs@technonjr.org>
2023-05-19 13:56:43 +0530

    Add files via upload

---
+++ Periodic Table.mm
@@ -0,0 +1,158 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Periodic Table" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1681974719883" BACKGROUND_COLOR="#ffff66" STYLE="oval" TEXT_SHORTENED="true">
+<arrowlink SHAPE="LINE" DESTINATION="ID_363111204" MIDDLE_LABEL="consists of" STARTINCLINATION="400 pt;0 pt;" ENDINCLINATION="414.5 pt;0 pt;"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_235808626" MIDDLE_LABEL="Consists of" STARTINCLINATION="227.5 pt;0 pt;" ENDINCLINATION="226.5 pt;0 pt;"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_972406990" MIDDLE_LABEL="Consists of" STARTINCLINATION="235.5 pt;0 pt;" ENDINCLINATION="241 pt;0 pt;"/>
+<font SIZE="18"/>
+<hook NAME="MapStyle" background="#ffffff" zoom="0.5">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1670402684000" fit_to_viewport="false" newNodeStyle=":style:defaultstyle.floating"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<hook URI="file:///D:/Simple_Periodic_Table_Chart-blocks.svg" SIZE="0.89153045" NAME="ExternalObject"/>
+<node TEXT="Noble Gases" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1942493472" CREATED="1681806705039" MODIFIED="1681974434871" HGAP_QUANTITY="748.5 pt" VSHIFT_QUANTITY="-294.5 pt" BACKGROUND_COLOR="#66ff66">
+<hook NAME="FreeNode"/>
+<node TEXT="Helium(He)" LOCALIZED_STYLE_REF="defaultstyle.floating" ID="ID_442620424" CREATED="1681971684462" MODIFIED="1681974429941" HGAP_QUANTITY="-57 pt" VSHIFT_QUANTITY="44 pt">
+<hook URI="file:///D:/external-Helium-periodic-table-bearicons-glyph-bearicons.png" SIZE="0.5520833" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="First Period" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_389875557" CREATED="1681970106117" MODIFIED="1681974406295" HGAP_QUANTITY="487.5 pt" VSHIFT_QUANTITY="-268 pt" BACKGROUND_COLOR="#66ff66">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Halogens" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_334286853" CREATED="1681806686071" MODIFIED="1681974442072" HGAP_QUANTITY="835.5 pt" VSHIFT_QUANTITY="-219.5 pt" BACKGROUND_COLOR="#66ff66">
+<hook NAME="FreeNode"/>
+<node TEXT="Chlorine(cl)" LOCALIZED_STYLE_REF="defaultstyle.floating" ID="ID_578747899" CREATED="1681971593453" MODIFIED="1681971891767" HGAP_QUANTITY="-37 pt" VSHIFT_QUANTITY="37 pt">
+<hook URI="file:///D:/external-Chlorine-periodic-table-bearicons-glyph-bearicons.png" SIZE="0.5416667" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Second period" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1499918608" CREATED="1681970125885" MODIFIED="1681974422751" HGAP_QUANTITY="547.5 pt" VSHIFT_QUANTITY="-213 pt" BACKGROUND_COLOR="#66ff66">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Periods(Rows)" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_972406990" CREATED="1681806131202" MODIFIED="1681974383723" HGAP_QUANTITY="331.5 pt" VSHIFT_QUANTITY="-168.5 pt" BACKGROUND_COLOR="#66ff66">
+<arrowlink SHAPE="LINE" DESTINATION="ID_1499918608"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_389875557"/>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Alkaline Metals" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_234607742" CREATED="1681806653807" MODIFIED="1681974445062" HGAP_QUANTITY="924 pt" VSHIFT_QUANTITY="-148 pt" BACKGROUND_COLOR="#66ff66">
+<hook NAME="FreeNode"/>
+<node TEXT="Mahnesium(Mg)" LOCALIZED_STYLE_REF="defaultstyle.floating" ID="ID_222707604" CREATED="1681971271459" MODIFIED="1681972037800" HGAP_QUANTITY="-66 pt" VSHIFT_QUANTITY="36.5 pt">
+<hook URI="file:///D:/external-Magnesium-periodic-table-bearicons-glyph-bearicons.png" SIZE="0.5416667" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Groups(Columns)&#xa;1-18" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_235808626" CREATED="1681806269963" MODIFIED="1681974407877" VGAP_QUANTITY="2.1 pt" HGAP_QUANTITY="349 pt" VSHIFT_QUANTITY="-85 pt" BACKGROUND_COLOR="#66ff66">
+<arrowlink SHAPE="LINE" FONT_SIZE="7" FONT_FAMILY="Sanskrit Text" DESTINATION="ID_1942493472" SOURCE_LABEL="&#xa;&#xa;" STARTINCLINATION="207 pt;13.5 pt;" ENDINCLINATION="199.5 pt;10.5 pt;"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_334286853"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_234607742" STARTINCLINATION="537.5 pt;0 pt;" ENDINCLINATION="537.5 pt;0 pt;"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_1214945603" STARTINCLINATION="490.5 pt;0 pt;" ENDINCLINATION="337.5 pt;0 pt;"/>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Alkali Metals" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1214945603" CREATED="1681806595856" MODIFIED="1681974451147" HGAP_QUANTITY="822 pt" VSHIFT_QUANTITY="-61.5 pt" BACKGROUND_COLOR="#66ff66">
+<hook NAME="FreeNode"/>
+<node TEXT="eg:- Sodium (Na)" LOCALIZED_STYLE_REF="defaultstyle.floating" ID="ID_868941651" CREATED="1681971196604" MODIFIED="1681972119314" HGAP_QUANTITY="-56 pt" VSHIFT_QUANTITY="39 pt">
+<hook URI="file:///D:/external-Sodium-periodic-table-bearicons-glyph-bearicons.png" SIZE="0.5208333" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Blocks&#xa;(s,p,d,f value of highest energy&#xa;occupied orbital.)" LOCALIZED_STYLE_REF="defaultstyle.floating" FOLDED="true" POSITION="right" ID="ID_363111204" CREATED="1681806220795" MODIFIED="1681974571410" HGAP_QUANTITY="375.5 pt" VSHIFT_QUANTITY="30.5 pt" BACKGROUND_COLOR="#66ff66">
+<arrowlink SHAPE="LINE" DESTINATION="ID_947926298" STARTINCLINATION="668.5 pt;0 pt;" ENDINCLINATION="643.5 pt;0 pt;"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_105677346" STARTINCLINATION="396 pt;0 pt;" ENDINCLINATION="654 pt;57.5 pt;"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_350002368" STARTINCLINATION="313 pt;0 pt;" ENDINCLINATION="448.5 pt;0 pt;"/>
+<arrowlink SHAPE="LINE" DESTINATION="ID_64645740" STARTINCLINATION="397 pt;0 pt;" ENDINCLINATION="396.5 pt;0 pt;"/>
+<hook NAME="FreeNode"/>
+<node TEXT="" ID="ID_1835763463" CREATED="1681963459745" MODIFIED="1681963459745"/>
+</node>
+<node TEXT="Actinides&#xa;(second f-block)" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_947926298" CREATED="1681964235994" MODIFIED="1681974693261" HGAP_QUANTITY="856.5 pt" VSHIFT_QUANTITY="38.5 pt" BACKGROUND_COLOR="#66ff66" TEXT_SHORTENED="true">
+<hook NAME="FreeNode"/>
+<hook URI="file:///D:/Actinide-1.jpg" SIZE="0.9222993" NAME="ExternalObject"/>
+</node>
+<node TEXT="Lanthanides&#xa;(first  f-block)" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_350002368" CREATED="1681964220395" MODIFIED="1681974719882" HGAP_QUANTITY="899.5 pt" VSHIFT_QUANTITY="77.5 pt" BACKGROUND_COLOR="#66ff66" TEXT_SHORTENED="true">
+<hook NAME="FreeNode"/>
+<hook URI="file:///D:/5000280.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="Transition Metals&#xa;(d-blocks)" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_64645740" CREATED="1681964214483" MODIFIED="1681974714241" HGAP_QUANTITY="911 pt" VSHIFT_QUANTITY="114.5 pt" BACKGROUND_COLOR="#66ff66" TEXT_SHORTENED="true">
+<hook NAME="FreeNode"/>
+<hook URI="file:///D:/Metallic-Character-Of-Transition-Metals-700x441.png" SIZE="0.85714287" NAME="ExternalObject"/>
+</node>
+<node TEXT="&quot;Representative Elements&quot;" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_105677346" CREATED="1681964230290" MODIFIED="1681974481333" HGAP_QUANTITY="414 pt" VSHIFT_QUANTITY="144 pt" BACKGROUND_COLOR="#ffff66" STYLE="oval">
+<arrowlink SHAPE="LINE" DESTINATION="ID_1486441638"/>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Main Group Elements" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1486441638" CREATED="1681964571770" MODIFIED="1681974492077" HGAP_QUANTITY="854 pt" VSHIFT_QUANTITY="193 pt" BACKGROUND_COLOR="#ffff66" STYLE="oval">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="energies of highest occupied&#xa;s,p and d Orbitals." LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1042788717" CREATED="1681964735843" MODIFIED="1681974505495" HGAP_QUANTITY="401.5 pt" VSHIFT_QUANTITY="299.5 pt" BACKGROUND_COLOR="#ffcc00" STYLE="wide_hexagon" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0" TEXT_SHORTENED="true">
+<arrowlink SHAPE="LINE" DESTINATION="ID_1103999504"/>
+<hook NAME="FreeNode"/>
+<hook URI="file:///D:/orb3d.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="Shell Model of the atom" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1103999504" CREATED="1681964615169" MODIFIED="1681974500509" HGAP_QUANTITY="763 pt" VSHIFT_QUANTITY="301.5 pt" BACKGROUND_COLOR="#ffff66" STYLE="oval">
+<arrowlink SHAPE="LINE" DESTINATION="ID_105677346" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="The most important ones are given the family names" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1254328008" CREATED="1681966517931" MODIFIED="1681966656013" HGAP_QUANTITY="393 pt" VSHIFT_QUANTITY="524 pt" BORDER_WIDTH="0 px" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+</map>
